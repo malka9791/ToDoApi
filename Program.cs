6a9cef7be1886a33c692c8105d1552a32877dbc6
malka9791 <m0527169791@gmail.com>
2025-02-04 22:10:39 +0200

    i hope it working

--- Program.cs
+++ Program.cs
@@ -20,13 +20,26 @@ builder.Services.AddDbContext<ToDoDbContext>(options =>
 //swagger
 builder.Services.AddEndpointsApiExplorer();
 
+builder.Services.AddSwaggerGen(options =>
+{
+    options.SwaggerDoc("v1", new OpenApiInfo
+    {
+        Title = "My API",
+        Version = "v1"
+    });
+});
+
 
 var app = builder.Build();
 //swagger
 //if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
-    app.UseSwaggerUI();
+    app.UseSwaggerUI(options =>
+    {
+        options.SwaggerEndpoint("/swagger/v1/swagger.json", "My API v1");
+        options.RoutePrefix = string.Empty;     });
+
 }
 //function
 
